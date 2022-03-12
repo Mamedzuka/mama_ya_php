6f5b0310b58a51cd6f99a47c4caa1e93dc32f4b3
Roman Mamedov <roman.mamedov.142000@gmail.com>
2022-03-12 19:19:41 +0300

    data init

---
+++ data/config.py
@@ -0,0 +1,21 @@
+from environs import Env
+
+env = Env()
+env.read_env()
+
+# Забираем токен как переменную среды
+BOT_TOKEN = env.str("BOT_TOKEN")
+# Если токен не найден - заканчиваем программу
+if BOT_TOKEN:
+    print('BOT_TOKEN found')
+else:
+    exit("Error: no BOT_TOKEN provided")
+
+# Забираем ID-шники как переменные среды
+ADMIN_IDS = env.list("ADMIN_IDS")
+# Если токен не найден - заканчиваем программу
+if ADMIN_IDS:
+    print('ADMIN_IDS are found')
+else:
+    exit("Error: no ADMIN_IDS are provided")
+
